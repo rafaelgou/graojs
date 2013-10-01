576cc8aee849a186205127fe55337bbbd87eec34
Marcelo M. Fleury <marcelomf@gmail.com>
2013-10-01 06:16:03 -0300

    Tests with objects and classes, Fleury slap

--- tests/objects.js
+++ tests/objects.js
@@ -1,32 +1,56 @@
+#!/usr/bin/env node
 /**
  * @author Pedro Nasser
+ *		   Marcelo M. Fleury
  */
+var maxMethods = 100000;
+var maxObjects = 10000000;
+var object;
 
-// PROTOTYPE 
-function Class () {}
-var m;
-for (m=0;m<100000;m++)
-Class.prototype['method'+m] = function () { console.log('lol'); };
- 
-console.time('prototype');
- 
-var i;
-for (i=0;i<10000000;i++)
-new Class;
- 
-console.timeEnd('prototype');
- 
-// OBJECT CREATE
- 
-var klass = {}
-var m;
-for (m=0;m<100000;m++)
-klass['method'+m] = function () { console.log('lol'); };
- 
-console.time('literal');
- 
-var i;
-for (i=0;i<10000000;i++)
-Object.create(klass);
- 
-console.timeEnd('literal');
+// Object Proto
+function ClassProto () {};
+
+console.time('ClassProtoMethods');
+for (var m = 0; m < maxMethods; m++)
+	ClassProto.prototype['method'+m] = function (o) { return 1+o; };
+console.timeEnd('ClassProtoMethods');
+
+console.time('ClassProtoObjects');
+for (var o = 0; o < maxObjects; o++)
+{
+	object = new ClassProto();
+	object.method99999(o);
+}
+console.timeEnd('ClassProtoObjects');
+
+// Object Literal
+var ClassLiteral = {};
+console.time('ClassLiteralMethods');
+for (var m = 0; m < maxMethods; m++)
+	ClassLiteral['method'+m] = function (o) { return 1+o; };
+console.timeEnd('ClassLiteralMethods');
+
+console.time('ClassLiteralObjects'); 
+for (var o = 0; o < maxObjects; o++)
+{
+	//object = Object.create(ClassLiteral);
+	//object.method99999(o);
+	ClassLiteral.method99999(o);
+}
+console.timeEnd('ClassLiteralObjects');
+
+// Object Function
+var ObjectFunction = function (){};
+
+console.time('ObjectFunctionMethods');
+for (var m = 0; m < maxMethods; m++)
+	ObjectFunction.prototype['method'+m] = function (o) { return 1+o; };
+console.timeEnd('ObjectFunctionMethods'); 
+
+console.time('ObjectFunctionObjects'); 
+for (var o = 0; o < maxObjects; o++)
+{
+	object = new ObjectFunction();
+	object.method99999(o);
+} 
+console.timeEnd('ObjectFunctionObjects');
