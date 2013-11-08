28723cd7ef96548653628519f88e29cf7012684c
Rafael Goulart <rafaelgou@gmail.com>
2013-11-08 16:43:24 -0200

    Adjust generator:app theme paths and some clean up

--- skeletons/schema/Schema.js
+++ skeletons/schema/Schema.js
@@ -1,6 +1,6 @@
-var {{ name | capitalize }}Schema = function(di) {
+var {{ schema | capitalize }}Schema = function(di) {
   validate = di.validate;
-  validator = di.validators.{{ name | lower }};
+  validator = di.validators.{{ schema | lower }};
 
   this.json = {
     id : di.mongoose.Schema.ObjectId
@@ -14,12 +14,18 @@ var {{ name | capitalize }}Schema = function(di) {
 //      index : true,
 //      unique : true,
 //      trim : true,
-//      validate : validator.validatorName
+//      validate : validator.validatorName,
+//      graoui: {
+//        label: 'Field Label',
+//        widget: 'input', // input, select, textarea
+//        inputtype: '', // For input: text, checkbox, radio, file, hidden, email, date, url
+//        options: { } // Any extra options - for SELECT/INPUT RADIO the option 'choices' are required
+//        attr: { } // Any extra html attributes, such 'class', 'style', 'placeholder', 'data-*', 'ng-*'
+//      }
 //    }
-
   };
 
   this.mongoose = new di.mongoose.Schema(this.json);
 };
 
-module.exports = exports = {{ name | capitalize }}Schema;
+module.exports = exports = {{ schema | capitalize }}Schema;
--- src/generator/GraoGenerator.js
+++ src/generator/GraoGenerator.js
@@ -5,222 +5,222 @@ var path = require('path') ,
     prompt = require('prompt');
 
 var walk = function (dir) {
-    var results = []
-    var list = fs.readdirSync(dir)
-    list.forEach(function (file) {
-        file = dir + '/' + file
-        var stat = fs.statSync(file)
-        if (stat && stat.isDirectory()) results = results.concat(walk(file))
-        else results.push(file)
-    })
-    return results;
+  var results = []
+  var list = fs.readdirSync(dir)
+  list.forEach(function (file) {
+    file = dir + '/' + file
+    var stat = fs.statSync(file)
+    if (stat && stat.isDirectory()) results = results.concat(walk(file))
+    else results.push(file)
+  })
+  return results;
 }
-
-//"pattern": "[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}",
-//"message": "Invalid email",
 
 var GraoGenerator = function () {
 
-    var self = this;
+  var self = this;
 
-    this.config = {};
-    this.defaults = {};
-    this.skelPath = null;
-    this.skelFilename = '';
-    this.skelDefaultFilename = 'skeleton.json';
-    this.currentDir = process.cwd();
-    this.defaultSkels = {
-        "app": "skeletons/app",
-        "bundle": "skeletons/bundle",
-        "schemabundle": "skeletons/schemabundle",
-        "schema": "skeletons/schema"
-    };
-    this.args = {};
-    this.argsSwig = {};
+  this.config = {};
+  this.defaults = {};
+  this.skelPath = null;
+  this.skelFilename = '';
+  this.skelDefaultFilename = 'skeleton.json';
+  this.currentDir = process.cwd();
+  this.defaultSkels = {
+    "app": "skeletons/app",
+    "bundle": "skeletons/bundle",
+    "schemabundle": "skeletons/schemabundle",
+    "schema": "skeletons/schema"
+  };
+  this.args = {};
+  this.argsSwig = {};
 
-    this.init = function (type, skeleton) {
-        this.skelPath = skeleton === null
-            ? path.join(__dirname, '/../../', this.defaultSkels[type])
-            : skeleton;
+  this.init = function (type, skeleton) {
+    this.skelPath = skeleton === null
+                  ? path.join(__dirname, '/../../', this.defaultSkels[type])
+                  : skeleton;
 
-        this.skelFilename = path.join(this.skelPath, this.skelDefaultFilename);
-        this.config = JSON.parse(
-            fs.readFileSync(this.skelFilename, 'utf8').toString().replace(/\n/g, '')
-        );
+    this.skelFilename = path.join(this.skelPath, this.skelDefaultFilename);
+    this.config = JSON.parse(
+      fs.readFileSync(this.skelFilename, 'utf8').toString().replace(/\n/g, '')
+    );
 
-        var defaultData = path.join(process.cwd(), '/config/default.skeleton.json');
-        this.defaults = fs.existsSync(defaultData)
-            ? JSON.parse(fs.readFileSync(defaultData))
-            : {};
+    var defaultData = path.join(process.cwd(), '/config/default.skeleton.json');
+    this.defaults = fs.existsSync(defaultData)
+                  ? JSON.parse(fs.readFileSync(defaultData))
+                  : {};
 
-        Object.keys(this.defaults).forEach(function (key) {
-            if (self.config.properties[key]) {
-                self.config.properties[key]['default'] = defaults[key];
-            }
-        });
-    }
+    Object.keys(this.defaults).forEach(function (key) {
+      if (self.config.properties[key]) {
+          self.config.properties[key]['default'] = defaults[key];
+      }
+    });
+  }
 
-    this.generate = function (args, force, callback) {
+  this.generate = function (args, force, callback) {
 
-        console.log("\n" + ( 'Loading from: ' + this.skelFilename ).green + "\n");
+    console.log("\n" + ( 'Loading from: ' + this.skelFilename ).green + "\n");
 
-        var tpls = this.prepareTplPaths(this.config, this.skelPath, args);
+    var tpls = this.prepareTplPaths(this.config, this.skelPath, args);
 
-        self.writeTpls(tpls, args, force, callback);
+    self.writeTpls(tpls, args, force, callback);
 
-    };
+  };
 
-    this.prepareTplPaths = function (config, sourcePath, args) {
+  this.prepareTplPaths = function (config, sourcePath, args) {
 
-        var sourceFiles = walk(sourcePath);
-        var tpls = {};
+    var sourceFiles = walk(sourcePath);
+    var tpls = {};
 
-        for (var i in sourceFiles) {
+    for (var i in sourceFiles) {
 
-            if (fs.statSync(sourceFiles[i]).isFile()) {
+      if (fs.statSync(sourceFiles[i]).isFile()) {
 
-                if (
-                    !this.checkIgnore(config.ignores, sourceFiles[i])
-                        && this.checkConditions(config.conditions, sourceFiles[i], args)
-                    ) {
+        if (
+          !this.checkIgnore(config.ignores, sourceFiles[i], sourcePath)
+              && this.checkConditions(config.conditions, sourceFiles[i], args)
+        ) {
 
-                    var file = this.rewrite(config.rewrites, sourceFiles[i]);
+          var file = this.rewrite(config.rewrites, sourceFiles[i], sourcePath);
 
-                    tpls[ sourceFiles[i] ] = this.swigRender(
-                        path.join(config.target, file.replace(sourcePath, '/')),
-                        args
-                    );
-
-                }
-
-            }
+          tpls[ sourceFiles[i] ] = this.swigRender(
+            path.join(config.target, file.replace(sourcePath, '/')),
+            args
+          );
 
         }
 
-        return tpls;
+      }
+
     }
 
-    this.writeTpls = function (tpls, args, force, callback) {
+    return tpls;
+  }
 
-        var skelPath = this.skelPath;
+  this.writeTpls = function (tpls, args, force, callback) {
 
-        Object.keys(tpls).forEach(function (tpl) {
+    var skelPath = this.skelPath;
 
-            var dist = tpls[tpl];
-            var distDir = path.dirname(dist);
+    Object.keys(tpls).forEach(function (tpl) {
 
-            if (fs.statSync(tpl).isFile()) {
+      var dist = tpls[tpl];
+      var distDir = path.dirname(dist);
 
-                fs.mkdirsSync(distDir);
-                fs.exists(process.cwd() + '/' + dist, function (exists) {
-
-                    if (!exists || force) {
-                        fs.writeFileSync(dist, self.swigRender(fs.readFileSync(tpl, 'utf-8'), args));
-                        console.log(( '+ ' + './' + dist ).green);
-                    } else {
-                        console.log(( '! ' + './' + dist ).red);
-                    }
-
-                });
-
-            } else {
-                this.writeDir(distDir, tpl, dist);
-            }
-        });
-
-        if (callback && typeof( callback ) === "function") {
-            callback(path.join(process.cwd(), args['app-name']), force);
-        }
-
-    };
-
-    this.writeDir = function (distDir, tpl, dist) {
+      if (fs.statSync(tpl).isFile()) {
 
         fs.mkdirsSync(distDir);
-        fs.copySync(tpl, dist);
+        fs.exists(process.cwd() + '/' + dist, function (exists) {
 
-        walk(dist).forEach(function (file) {
-
-            file = path.join(dist, file);
-            fs.writeFileSync(file, swig.render(fs.readFileSync(file, 'utf-8'), swig_result), 'utf-8');
+          if (!exists || force) {
+            fs.writeFileSync(dist, self.swigRender(fs.readFileSync(tpl, 'utf-8'), args));
+            console.log(( '+ ' + './' + dist).green);
+          } else {
+            console.log(( '! ' + './' + dist ).red);
+          }
 
         });
-    };
 
-    this.swigRender = function (content, args) {
+      } else {
+          this.writeDir(distDir, tpl, dist);
+      }
+    });
 
-        Object.keys(args).forEach(function (arg) {
-            self.argsSwig[ arg.replace('-', '_') ] = args[arg];
-        });
-
-        var locals = { locals: this.argsSwig };
-
-        return swig.render(content, locals);
+    if (callback && typeof( callback ) === "function") {
+      callback(path.join(process.cwd(), args['name']), force);
     }
 
-    this.checkIgnore = function (ignores, file) {
+  };
 
-        for (var i in ignores) {
+  this.writeDir = function (distDir, tpl, dist) {
 
-            var pattern = "^" + ignores[i].replace(/\//g, "\\/").replace(/\./g, "\\.") + ".*";
-            var regex = RegExp(pattern);
+    fs.mkdirsSync(distDir);
+    fs.copySync(tpl, dist);
 
-            if (file.match(regex)) {
-                return true;
+    walk(dist).forEach(function (file) {
+
+      file = path.join(dist, file);
+      fs.writeFileSync(file, swig.render(fs.readFileSync(file, 'utf-8'), swig_result), 'utf-8');
+
+    });
+  };
+
+  this.swigRender = function (content, args) {
+
+    Object.keys(args).forEach(function (arg) {
+      self.argsSwig[ arg.replace('-', '_') ] = args[arg];
+    });
+
+    var locals = { locals: this.argsSwig };
+
+    return swig.render(content, locals);
+  }
+
+  this.checkIgnore = function (ignores, file, sourcePath) {
+
+    file = file.replace(sourcePath + '/', '');
+
+    for (var i in ignores) {
+
+      var pattern = "^" + ignores[i].replace(/\//g, "\\/").replace(/\./g, "\\.") + ".*";
+      var regex = RegExp(pattern);
+
+      if (file.match(regex)) {
+          return true;
+      }
+
+    }
+
+    return false;
+
+  }
+
+  this.rewrite = function (rewrites, file, sourcePath) {
+
+    file = file.replace(sourcePath + '/', '');
+
+    if (rewrites.hasOwnProperty(file)) {
+        return rewrites[ file ];
+    } else {
+        return file;
+    }
+
+  }
+
+  this.checkConditions = function (conditions, file, args) {
+
+    for (var i in conditions) {
+
+      var condition = conditions[i];
+
+      for (var j in condition.matches) {
+
+        var pattern = "^" + condition.matches[j].replace(/\//g, "\\/").replace(/\./g, "\\.") + ".*";
+        var regex = RegExp(pattern);
+        if (file.match(regex)) {
+
+          var rule = true;
+
+          for (var j in condition.rules) {
+
+            var rl = condition.rules[j];
+            if (rl.value !== args[ rl.arg ]) {
+              rule = false;
+              break;
             }
 
+          }
+          return rule;
         }
-
-        return false;
-
+      }
     }
 
-    this.rewrite = function (rewrites, file) {
-
-        if (rewrites.hasOwnProperty(file)) {
-            return rewrites[ file ];
-        } else {
-            return file;
-        }
-
-    }
-
-    this.checkConditions = function (conditions, file, args) {
-
-        for (var i in conditions) {
-
-            var condition = conditions[i];
-
-            for (var j in condition.matches) {
-
-                var pattern = "^" + condition.matches[j].replace(/\//g, "\\/").replace(/\./g, "\\.") + ".*";
-                var regex = RegExp(pattern);
-                if (file.match(regex)) {
-
-                    var rule = true;
-
-                    for (var j in condition.rules) {
-
-                        var rl = condition.rules[j];
-                        if (rl.value !== args[ rl.arg ]) {
-                            rule = false;
-                            break;
-                        }
-
-                    }
-                    return rule;
-                }
-            }
-        }
-        return true;
-    }
-
-
+    return true;
+  }
 }
 
 function onErr(err) {
-    console.log(err);
-    return 1;
+  console.log(err);
+  return 1;
 }
 
 module.exports = exports = GraoGenerator;
