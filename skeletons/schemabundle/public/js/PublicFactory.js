5fd01feb62fe1e6fdb77321cd2b8f5d52354a11e
Marcelo M. Fleury <marcelomf@gmail.com>
2013-12-06 15:16:59 -0200

    Now, we have a new stable generator

---
+++ skeletons/schemabundle/public/js/PublicFactory.js
@@ -0,0 +1,8 @@
+GraoJS.factory('{{ schema | lower }}', ['$resource', 'config', function($resource, config) {
+    var {{ schema | lower }} = $resource(config.baseUrl+'/{{ schema | lower }}/:id', { id: '@_id' }, {
+        update: {
+            method: 'PUT'
+        }
+    });
+    return {{ schema | lower }};
+}]);
